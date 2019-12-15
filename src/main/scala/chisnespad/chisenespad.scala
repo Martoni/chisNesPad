d8d67a57c6a0591ccb2bf7462d552456af8c3205
Fabien Marteau <fabien.marteau@armadeus.com>
2019-12-15 15:39:20 +0100

    compile but not tested version of chisNesPad

--- src/main/scala/chisnespad/chisenespad.scala
+++ src/main/scala/chisnespad/chisenespad.scala
@@ -4,7 +4,9 @@ import chisel3._
 import chisel3.util._
 import chisel3.Driver
 
-class ChisNesPad extends Module {
+class ChisNesPad (val mainClockFreq: Int = 100,
+                  val clockFreq: Int = 1,
+                  val regLen: Int = 16)extends Module {
   val io = IO(new Bundle{
     /* SNES Pinout */
     val dclock = Output(Bool())
@@ -14,12 +16,76 @@ class ChisNesPad extends Module {
     val data = Decoupled(Output(UInt(16.W)))
   })
 
+  /* counter declarations*/
+  val maxCount = mainClockFreq/clockFreq
+  val halfMaxCount = maxCount/2
+  val countReg = RegInit(0.U(log2Ceil(maxCount + 1).W))
+  val fallReg = RegInit(false.B)
+  val riseReg = RegInit(false.B)
+
+  /* shift register (pad) declarations */
+  val regCount = RegInit(regLen.U(5.W))
+  val padReg = RegInit(0.U(regLen.W))
+
+  /* State machine declarations */
+  val sinit::schigh::sclow::svalid::Nil = Enum(4)
+  val stateReg = RegInit(sinit)
+
+  /* counter */
+  riseReg := 0.U
+  fallReg := 0.U
+  when(stateReg === sclow || stateReg === schigh){
+    when(countReg >= maxCount.U){
+      countReg := 0.U
+      riseReg := 1.U
+    }.elsewhen(countReg === halfMaxCount.U){
+      fallReg := 1.U
+      countReg := countReg + 1.U
+    }.otherwise{
+      countReg := countReg + 1.U
+    }
+  }
+
+  /* reg shift */
+   when(riseReg){
+      regCount := regCount - 1.U
+      padReg := padReg(regLen - 2, 0) ## io.sdata
+   }
   
 
-  io.dclock := DontCare
-  io.dlatch := DontCare
-  io.data.bits := DontCare
-  io.data.valid := DontCare
+  /* state machine */
+  io.data.valid := 0.U
+  switch(stateReg){
+    is(sinit){
+      when(io.data.ready){
+        stateReg := schigh
+        countReg := 0.U
+      }
+    }
+    is(sclow){
+      when(riseReg){
+        stateReg := schigh
+      }
+
+    }
+    is(schigh){
+      when(fallReg){
+        when(regCount =/= 0.U){
+          stateReg := sclow
+        }.otherwise{
+          stateReg := svalid
+        }
+      }
+    }
+    is(svalid){
+      stateReg := sinit
+      io.data.valid := 1.U
+    }
+  }
+
+  io.dclock := (stateReg === schigh)
+  io.dlatch := (stateReg === sinit)
+  io.data.bits := padReg
 }
 
 object ChisNesPad extends App {
